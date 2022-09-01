9fcc1173f78f2fe2889b77dabfa6220a26d1265a
AlfiyaShaikh-10 <alfiyashaikh0777@gmail.com>
2022-09-01 19:28:00 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            NewsMonkey
+            NewsBee
           </Link>
           <button
             className="navbar-toggler"
